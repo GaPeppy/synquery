7ff46b54a55a64669cd82b0f50b12882d77a42bb
GaPeppy <>
2022-03-04 14:13:59 -0500

    few more metrics and splitting data collection to multiple graphql calls

--- RollupReporting.js
+++ RollupReporting.js
@@ -37,13 +37,14 @@ function CaptureCustomAttribute(sMCred,sTag,sValue){
 //
 //
 //
-function zippayload(sTable,ntimestamp,aEvents){
+function zippayload(sTable, ntimestamp, sprovider, aEvents){
   return new Promise((resolve, reject) => {
     if(!Array.isArray(aEvents) || aEvents.length == 0){return reject('array is empty')}
     logit('zippayload','about to create number of events',aEvents.length,sTable)
     for (var oa of aEvents){
       oa.eventType = sTable
       oa.timestamp = ntimestamp
+      oa.provider = sprovider
     }
     zlib.gzip(JSON.stringify(aEvents), null, function (err, compressed_json) {
       if(err) {logit('zippayload','gzip bombed',err); return reject(err)}
@@ -95,8 +96,8 @@ function GetAccounts(sCred){
 //
 // GetCounts() -> run parallel queries for a target subaccount
 //
-function GetCounts(sCred,oAcct,nBatchID){
-  const METHODNAME = 'GetCounts'
+function GetCounts1(sCred,oAcct,nBatchID, oResult){
+  const METHODNAME = 'GetCounts1'
   const CQuery = `
 {
   actor {
@@ -116,7 +117,7 @@ function GetCounts(sCred,oAcct,nBatchID){
       ec2uc: nrql(query: "SELECT uniqueCount(entityGuid) as uc FROM ComputeSample WHERE provider = 'Ec2Instance' SINCE 1 day ago") {
         results
       }
-      k8snodeuc: nrql(query: "FROM K8sNodeSample SELECT uniqueCount(hostname) as uc SINCE 1 day ago") {
+      k8snodeuc: nrql(query: "FROM K8sNodeSample SELECT uniqueCount(entityGuid) as uc SINCE 1 day ago") {
         results
       }
       iauc: nrql(query: "SELECT uniqueCount(entityGuid) as uc FROM SystemSample SINCE 1 day ago") {
@@ -165,20 +166,31 @@ function GetCounts(sCred,oAcct,nBatchID){
   return NerdGraph(sCred,CQuery.replace(/RPMID/g,oAcct.id),'').then((payload)=>{
     return new Promise((resolve,reject)=>{
       try{
-        var cres = {}
-        cres.ucresult = {NrAccountId:oAcct.id, NrAccountName:oAcct.name, ReportingPeriod:'1d', BatchId:nBatchID.toString()}
-        cres.ucresult["Metadata.Dashboard.Count"]  = payload.data.actor.dbcount.count
-        cres.ucresult["Metadata.InfraAgent.Count"] = payload.data.actor.iacount.count
-        cres.ucresult["InfraAgent.UCount"]         = payload.data.actor.account.iauc.results[0].uc
-        cres.ucresult["InfraAgent.K8sNode.UCount"] = payload.data.actor.account.k8snodeuc.results[0].uc
-        cres.ucresult["Serverless.Lambda.UCount"]  = payload.data.actor.account.lambdauc.results[0].uc
-        cres.ucresult["Cloudwatch.Lambda.UCount"]  = payload.data.actor.account.lambdaciuc.results[0].uc
-        cres.ucresult["Cloudwatch.EC2.UCount"]     = payload.data.actor.account.ec2uc.results[0].uc
-        cres.ucresult["Incident.Open.UCount"]      = payload.data.actor.account.aiiopenuc.results[0].uc
-        cres.ucresult["Incident.Close.UCount"]     = payload.data.actor.account.aiicloseuc.results[0].uc
-        cres.ucresult["Metric.PeakDPM"]            = payload.data.actor.account.peakdailydpm.results[0].peakdailydpm
-        cres.ucresult["Show.EventTypes.Count"]     = payload.data.actor.account.showeventtypes.results.length
-        cres.ucresult["CloudIntegrations.LinkedAccounts.Count"] = payload.data.actor.account.cloud.linkedAccounts.length
+        oResult["NrAccountId"]     = oAcct.id
+        oResult["NrAccountName"]   = oAcct.name
+        oResult["ReportingPeriod"] = '1d'
+        oResult["BatchId"]         = nBatchID.toString()
+        oResult["Metadata.Dashboard.Count"]  = payload.data.actor.dbcount.count
+        oResult["Metadata.InfraAgent.Count"] = payload.data.actor.iacount.count
+        oResult["InfraAgent.UCount"]         = payload.data.actor.account.iauc.results[0].uc
+        oResult["InfraAgent.K8sNode.UCount"] = payload.data.actor.account.k8snodeuc.results[0].uc
+        oResult["Serverless.Lambda.UCount"]  = payload.data.actor.account.lambdauc.results[0].uc
+        oResult["Cloudwatch.Lambda.UCount"]  = payload.data.actor.account.lambdaciuc.results[0].uc
+        oResult["Cloudwatch.EC2.UCount"]     = payload.data.actor.account.ec2uc.results[0].uc
+        oResult["Incident.Open.UCount"]      = payload.data.actor.account.aiiopenuc.results[0].uc
+        oResult["Incident.Close.UCount"]     = payload.data.actor.account.aiicloseuc.results[0].uc
+        oResult["Metric.PeakDPM"]            = payload.data.actor.account.peakdailydpm.results[0].peakdailydpm
+        oResult["Show.EventTypes.Count"]     = payload.data.actor.account.showeventtypes.results.length
+        oResult["CloudIntegrations.LinkedAccounts.Count"] = payload.data.actor.account.cloud.linkedAccounts.length
+        //loop over limits and capture the count of limit events in last 24hours
+        /**
+      limiterrors: nrql(query: "select count(*) from NrIntegrationError where category = 'RateLimit' facet limitName limit 100 since 1 day ago") {
+        results
+      }
+        payload.data.actor.account.limiterrors.results.forEach( el => {
+          cres.ucresult["RateLimit." + el.limitName] = el.count
+        })
+        **/
         var nServices = 0
         payload.data.actor.account.cloud.linkedAccounts.forEach( el => {
           if(el.disabled == false){
@@ -189,13 +201,55 @@ function GetCounts(sCred,oAcct,nBatchID){
             })
           }
         })
-        cres.ucresult["CloudIntegrations.Services.Count"] = nServices
+        oResult["CloudIntegrations.Services.Count"] = nServices
 
         //logit(METHODNAME,'debug cres',cres)
-        return resolve(cres)
+        return resolve(oResult)
       }
       catch (e) {
-        logit('GetCounts','caught exception',oAcct,e,payload)
+        logit(METHODNAME,'caught exception',oAcct,e,payload)
+        return reject(e)
+      }
+    })
+  })
+}
+//
+// GetCounts() -> run parallel queries for a target subaccount
+//
+function GetCounts2(sCred,oAcct,nBatchID, oResult){
+  const METHODNAME = 'GetCounts2'
+  const CQuery = `
+{
+  actor {
+    account(id: RPMID) {
+      limiterrors: nrql(query: "select count(*) from NrIntegrationError where category = 'RateLimit' facet limitName limit 100 since 1 day ago") {
+        results
+      }
+      logcount: nrql(query: "select count(*) from Log since 1 day ago") {
+        results
+      }
+    }
+  }
+}`
+
+  return NerdGraph(sCred,CQuery.replace(/RPMID/g,oAcct.id),'').then((payload)=>{
+    return new Promise((resolve,reject)=>{
+      try{
+        oResult["NrAccountId"]     = oAcct.id
+        oResult["NrAccountName"]   = oAcct.name
+        oResult["ReportingPeriod"] = '1d'
+        oResult["BatchId"]         = nBatchID.toString()
+        oResult["Log.Count"]       = payload.data.actor.account.logcount.results[0].count
+        //loop over limits and capture the count of limit events in last 24hours
+        payload.data.actor.account.limiterrors.results.forEach( el => {
+          oResult["RateLimit." + el.limitName] = el.count
+        })
+
+        //logit(METHODNAME,'debug cres',cres)
+        return resolve(oResult)
+      }
+      catch (e) {
+        logit(METHODNAME,'caught exception',oAcct,e,payload)
         return reject(e)
       }
     })
@@ -204,10 +258,13 @@ function GetCounts(sCred,oAcct,nBatchID){
 //
 // LoopAccounts() -> scatter and gather for current MasterContext; parallel execution
 //
-function LoopAccounts(sCred, aAccounts, nBatchID){
+function LoopAccounts(sCred, aAccounts, nBatchID, aResults){
   parray = []
   for(oa of aAccounts){
-    parray.push(GetCounts(sCred,oa,nBatchID))
+    var oResult = {}
+    aResults.push(oResult)
+    parray.push(GetCounts1(sCred,oa,nBatchID, oResult))
+    parray.push(GetCounts2(sCred,oa,nBatchID, oResult))
   }
   return Promise.all(parray)
 }
@@ -230,26 +287,28 @@ function logit(mname,msg, ...theargs){
 }
 
 function ProcessMasterContext(sMasterCred, nBatchID, sTargetTable){
+  const METHODNAME = 'ProcessMasterContext'
   var nstrt = Date.now()
+  var aAccountResponseObjects = []
   return GetAccounts(sMasterCred)
   .then((aAccounts) =>{
     //console.log('GetAccounts() result:',payload)
     logit('ProcessMasterContext','working on number of accounts',aAccounts.length)
-    //logit('main','aAccounts',aAccounts)
-    return LoopAccounts(sMasterCred, aAccounts, nBatchID)
-  }).then((aCounts)=>{
-    var uCounts = []
-    aCounts.forEach(el => {uCounts.push(el.ucresult)})
-    return zippayload(sTargetTable,nBatchID,uCounts)
+    //logit(METHODNAME,'aAccounts',aAccounts)
+    // called methods will populate the array -> aAccountResponseObjects
+    return LoopAccounts(sMasterCred, aAccounts, nBatchID, aAccountResponseObjects)
+  }).then(()=>{
+    logit(METHODNAME,'final acct array',aAccountResponseObjects)
+    return zippayload(sTargetTable, nBatchID, 'eroll', aAccountResponseObjects)
   }).then((zp) => {
     return PostZipEvents($secure.POA_INGEST_KEY,$secure.POA_ACCOUNT_ID,zp)
     .then((response1)=>{
-      logit('main','postevents-response1',response1.statusCode)
+      logit(METHODNAME,'postevents-response1',response1.statusCode)
     })
   }).then(()=>{
-    logit('ProcessMasterContext','iteration stopwatch in secs',(Date.now()-nstrt)/1000)
+    logit(METHODNAME,'iteration stopwatch in secs',(Date.now()-nstrt)/1000)
   }).catch(err => {
-    logit('ProcessMasterContext','caught error on promise-chain',err)
+    logit(METHODNAME,'caught error on promise-chain',err)
     assert.fail(err)
   })
 }
